c2f1bee6474f7748048371119d17beba5e949b39
Steve <steve@kitkat@gmail.com>
2024-06-03 23:22:31 +0700

    initial commit

---
+++ composeApp/src/commonMain/kotlin/data/remote/api/CurrencyApiService.kt
@@ -0,0 +1,9 @@
+package data.remote.api
+
+class CurrencyApiService {
+
+    companion object {
+        const val ENDPOINT = "https://api.currencyapi.com/v3/latest"
+        const val API_KEY = "cur_live_5TjtBbUjAMaDIJBtqdqqjxwQEyW8Eq4EQDB0uDO1"
+    }
+}
